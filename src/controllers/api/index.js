6f79acd0abc507a2aca1ef84dae1a05bd33def71
Pablo Monteserín <paramonty@gmail.com>
2020-12-08 19:33:15 +0100

    subida

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -2,9 +2,6 @@ import UsersController from './users';
 import AuthController from './auth';
 
 export default function (app) {
-  // Public
-  app.use('/auth', AuthController);
-
   // Private
   app.use('/users', UsersController);
 
