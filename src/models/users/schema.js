a00cf2832e19a1e7a51159a188c050496bebbab4
pablomonteserin@pablomonteserin.com <pablomonteserin@pablomonteserin.com>
2021-03-18 13:18:20 +0100

    subida

--- src/models/users/schema.js
+++ src/models/users/schema.js
@@ -1,10 +1,8 @@
 import { db } from '../../application/database';
 const { DataTypes } = require('sequelize');
 
-const User = db.define('user', {
+export default db.define('user', {
 	username: DataTypes.STRING,
 	password: DataTypes.STRING,
 	mail: DataTypes.STRING,
 });
-
-export default User;
